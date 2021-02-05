56c2e0051a9a2da981f669498e88b23ccc4c30ff
apsius <38158216+appsius@users.noreply.github.com>
2021-02-05 21:15:29 +0300

    passing arguments to event handlers

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScroll = document.querySelector('.btn--scroll-to');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab--container');
+const content = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,7 +35,6 @@ document.addEventListener('keydown', function (e) {
 });
 
 // SMOOTH SCROLLING
-const btnScroll = document.querySelector('.btn--scroll-to');
 btnScroll.addEventListener('click', e => {
   const s1 = document.querySelector('#section--1');
   s1.scrollIntoView({ behavior: 'smooth' });
@@ -46,13 +50,8 @@ document.querySelector('.nav__links').addEventListener('click', e => {
 });
 
 // TAB FUNCTIONALITY
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab--container');
-const content = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   e.preventDefault();
-
   // Find active tab
   const clicked = e.target.closest('.operations__tab');
 
@@ -69,3 +68,23 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// HOVER EFFECT
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('nav').querySelectorAll('.nav__link');
+    const logo = link.closest('nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
