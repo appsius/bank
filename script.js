22bc97796dc23413bbbac3371562de55587fd4eb
apsius <38158216+appsius@users.noreply.github.com>
2021-02-06 15:07:54 +0300

    developped stick nav

--- script.js
+++ script.js
@@ -90,9 +90,20 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 // Sticky navigation
-const initialCoords = section1.getBoundingClientRect();
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+const stickifyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickifyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+
+headerObserver.observe(header);
